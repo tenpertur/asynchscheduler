c38623fda08161c8b9f49db5dbc1e5b15f5eb14c
tenpertur <tenpertur@gmail.com>
2022-08-28 13:33:48 +0200

    initial commit

---
+++ src/test/java/com/github/tenpertur/asynchscheduler/AsynchschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tenpertur.asynchscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsynchschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
